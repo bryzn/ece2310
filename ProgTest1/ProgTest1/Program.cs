ae4d90cb021a6d72afcacec8b86dbf97f70726f4
Bryan Antonio Arciniega <arciniegabryan@gmail.com>
2019-02-15 22:46:46 -0800

    Uploading Old Projects

---
+++ ProgTest1/ProgTest1/Program.cs
@@ -0,0 +1,69 @@
+using System;
+
+namespace ProgTest1
+{
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Please enter a number x:");
+                double x = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("exp({0}) = " + CalculateExp(x), x);
+
+                Console.Read();
+            }
+
+
+
+            static double CalculateExp(double x)
+            {
+
+                double answer = 0; //holds and returns the final answer
+
+                double oldanswer = 0; //is used to hold the previous answer and will be used to stop the taylor series
+
+                double k = 0;
+
+                while (true)
+                {
+
+                    answer += Math.Pow(x, k) / Factorial(k);
+
+                    if (answer == oldanswer)
+                    {
+                        break; // once the previous answer and new answer are the same there is no need to continue the taylor series
+                    }
+                    else
+                    {
+                        oldanswer = answer;
+                        k++; // is used to track the iterations
+                    }
+
+                }
+
+                return answer;
+            }
+
+
+
+            static double Factorial(double x)
+            {
+
+                double answer = 1;
+                double counter = 1;
+
+                while (counter <= x)
+                {
+                    answer = answer * counter;
+                    counter++;
+
+                }
+
+                return answer;
+            }
+
+        }
+    }
+
---
+++ TimeManagement/TimeManagement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TimeManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time lunchtime = new Time(); //instantiating the new object named lunch time
+            lunchtime.set_time(13, 10, 00);
+            lunchtime.get_time();
+            lunchtime.display_time();
+            //creating a new object named lunchtime2
+            Time lunchtime2 = lunchtime.get_time();
+            lunchtime2.display_time();
+            Time dinnertime = new Time(18, 0, 0);
+            dinnertime.display_time();
+            dinnertime.set_time(25, 60, 60);
+            dinnertime.display_time();
+            //creating an object with third constructor
+            Time newtime = new Time(5);
+            newtime.display_time();
+
+
+        }
+    }
+}
