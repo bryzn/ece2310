ca698506a03a7354ac1665613d448e7529b08e33
Bryan Antonio Arciniega <arciniegabryan@gmail.com>
2019-02-15 22:35:59 -0800

    Uploaded the Date Management program

---
+++ exercise_5_2_3/exercise_5_2_3/Date.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace exercise_5_2_3
+{
+    public class Date
+    {
+        int month;
+        int day;
+        int year;
+        public Date() //constructor and setting the variables to 0
+        {
+            month = 0;
+            day = 0;
+            year = 0;
+        }
+        public Date(int m, int d, int y)
+        {
+            set_date(m, d, y);
+        }
+        public void set_date(int m, int d, int y)
+        {
+            month = m;
+            day = d;
+            year = y;
+        }
+        public Date get_date()
+        { return this; }
+        public void display_date()
+        {
+            Console.WriteLine("\n{0}-{1}-{2}\n", month, day, year);
+        }
+    }
+}
---
+++ exercise_5_2_3/exercise_5_2_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercise_5_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the date project\n");
+            Date birthdate = new Date(3, 14, 1998);
+            birthdate.set_date(3, 15, 1998);
+            birthdate.display_date();
+            //this will be my anniversary date
+            Date anniversaryDate = new Date(01, 19, 2016);
+            anniversaryDate.display_date();
+            Date newAnniversaryDate = anniversaryDate.get_date();
+            anniversaryDate.set_date(0, 0, 000);
+            Console.Write("Unfortunately I do not have an anniversary date anymore :(\n");
+            newAnniversaryDate.display_date();
+        }
+    }
+}
