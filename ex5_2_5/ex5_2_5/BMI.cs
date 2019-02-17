b3b7fb67b2eab8e3e14e63d82b957bdc99a92991
Bryan Antonio Arciniega <arciniegabryan@gmail.com>
2019-02-17 15:21:30 -0800

    BMI Project

---
+++ ex5_2_5/ex5_2_5/BMI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ex5_2_5
+{
+    public class BMI
+    {
+        double weight;
+        double height;
+        public double BMImeasure;
+        public BMI()
+        {
+            weight = 1;
+            height = 1;
+            calculateBMI();
+        }
+        public BMI(double inweight, double inheight)
+        {
+            set_values(inweight, inheight);
+            calculateBMI();
+        }
+        private void calculateBMI()
+        {
+            BMImeasure = 703 * (weight / (height *height));
+        }
+        public void set_values(double inweight, double inheight)
+        {
+            weight = inweight;
+            height = inheight;
+            calculateBMI();
+
+        }
+        public void print()
+        {
+            Console.Write("\nThe BMI is: {0}\n", BMImeasure);
+        }
+
+    }
+}
---
+++ ex5_2_5/ex5_2_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex5_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMI Eric = new BMI();
+            Eric.set_values(150, 72);
+            Eric.print();
+            BMI Alice = new BMI(120, 60);
+            Alice.print();
+            Console.WriteLine(Math.Exp(1.0));
+
+       
+
+        }
+    }
+}
