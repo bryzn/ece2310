6f627988aeada9ef1fb84accf3764294e2c5a129
Bryan Antonio Arciniega <arciniegabryan@gmail.com>
2019-02-17 19:25:26 -0800

    Finished right triangle smple test

---
+++ sampleTest/sampleTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace sampleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RightTriangle rt1 = new RightTriangle();
+            RightTriangle rt2 = new RightTriangle(3, 4);
+            rt2.toString();
+            rt1.set_values(6, 8);
+            rt1.toString();
+        }
+    }
+}
---
+++ sampleTest/sampleTest/RightTriangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace sampleTest
+{
+    public class RightTriangle
+    {
+        double sideA, sideB, hypotenuse, area, perimeter;
+        public RightTriangle()
+        {
+            sideA = 1;
+            sideB = 1;
+            calculate();
+        }
+        public RightTriangle(double insideA, double insideB)
+        {
+            set_values(insideA, insideB);
+            calculate();
+        }
+        private void calculate()
+        {
+            area = sideA * sideB;
+            perimeter = (sideA * sideA) + (sideB * sideB);
+            hypotenuse = Math.Sqrt(perimeter);
+        }
+        public void set_values(double insideA, double insideB)
+        {
+            sideA = insideA;
+            sideB = insideB;
+        }
+        public void toString()
+        {
+            Console.WriteLine("SideA: " + sideA + "\nSideB: " + sideB +
+            "\nHypotenuse: " + hypotenuse + "\nArea: " + area + "\nPerimeter: " + perimeter);
+        }
+    }
+}
