dff1d6afdb4a867f709a5def6e400557fc888aa4
Bryan Antonio Arciniega <baarciniega@cpp.edu>
2019-02-26 16:50:13 -0800

    Add files via upload
    Static Class member example

---
+++ ProgTest2/ProgTest2/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgTest2
+{
+    public class Student
+    {
+        public static int Count
+        {get; set;}
+        public string FirstName { get; set; }
+        public string LastName { get; private set; }
+        public Student(string f, string l)
+        {
+            FirstName = f;
+            LastName = l;
+            ++Count; //maintain the public class member count
+            Console.WriteLine("Student Constructor: {0} {1}; Count = {2}", FirstName, LastName, Count);
+        }
+
+
+    }
+}
